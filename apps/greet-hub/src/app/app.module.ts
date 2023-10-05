c7288eff608186b9e29fc50b05dd4f7a37578007
Frederic Mandrea <fred@notanumber.fr>
2023-10-05 13:24:24 +0200

    Init angular material with menu bar

--- apps/greet-hub/src/app/app.module.ts
+++ apps/greet-hub/src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, HomeComponent, EventsComponent],
   imports: [
     BrowserModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/greet-hub/src/app/app.routes.ts
+++ apps/greet-hub/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';  
+import { EventsComponent } from './events/events.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'events',
+        component: EventsComponent
+    },
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
